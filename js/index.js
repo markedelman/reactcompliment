20ba271de346b1b2cfee5a9859212d6a2ac44800
Mark Edelman <markedelman1987@gmail.com>
2017-01-11 12:19:08 -0500

    index.js file

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDom from 'react-dom';
+
 require('babel-polyfill');
+
+document.addEventListener('DOMContentLoaded', () => {
+ReactDom.render(
+
+// some stuff will have to go here
+// like state or parent component
+//
+
+    document.getElementById('application')
+);
+});
+
 
