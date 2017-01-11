ac3e6c52e206387c12993ac0efbef7501c9d17e8
Mark Edelman <markedelman1987@gmail.com>
2017-01-11 17:14:26 -0500

    after Alex's help and we have a working prototype

--- js/components/submit.js
+++ js/components/submit.js
@@ -7,30 +7,63 @@ import Display from './display';
 export default class Submit extends React.Component {
 constructor(props){
     super(props);
-    this.state = {clicked: false}; 
+    this.state = {
+        clicked: false,
+        compliments: ["You are a better programmer than Ryan Dahl", "You have better ", "You are MORE really really really ridiculously good looking than Derek Zoolander"]
+    }; 
     this.onButtonClick =this.onButtonClick.bind(this);
-};
+}
+
 onButtonClick(){
     console.log("On button click");
     this.setState({clicked: true});
-};
-
-//["","",""]
-render(){
-
- var showCompliment=[];
-     if(this.state.clicked){
-       showCompliment =['hi','hello'];
-     }
-return(
-  
-   <div>
-    <Display onClick={this.onButtonClick} text="showCompliment"/>
-    {showCompliment}
-    </div>
- );
 }
-};
+
+// 
+
+
+
+
+// const showCompliment = () => {
+// return compliments[Math.floor((Math.random() * 3))];
+// };
+
+showCompliment(){
+    return this.state.compliments[Math.floor((Math.random() * 3))]
+}
+
+startApp() {
+        return (
+            <div>
+                <Display onClick={this.onButtonClick} text="showCompliment"/>
+            </div>
+        )
+}
+
+
+appClicked(){
+    return (
+        <div>
+            <Display onClick={this.onButtonClick} text="showCompliment"/>
+            {this.showCompliment()}
+        </div>
+    )
+}
+
+render(){
+// const compliments = ["You are a better programmer than Ryan Dahl", "You have better ", "You are MORE really really really ridiculously good looking than Derek Zoolander"];
+// const showCompliment = () => {
+// return compliments[Math.floor((Math.random() * 3))];
+//  var showCompliment;
+     if(!this.state.clicked){
+         return this.startApp();
+     } else {
+         return this.appClicked();
+     }
+    }
+
+}
+
 
 // class Surprise extends React.Component {
 //     constructor(props) {
