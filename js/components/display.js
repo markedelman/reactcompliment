b39a72d5d607d55b54386ffdf7bc379398051ba6
Mark Edelman <markedelman1987@gmail.com>
2017-01-12 09:46:56 -0500

    last two lines to modify

--- js/components/display.js
+++ js/components/display.js
@@ -3,11 +3,11 @@
 import React from 'react';
 
 export default function Display(props){
-  
+  console.log(props);
     
     return(
         <div className="display">
-            <button type="button" onClick={props.onClick}>Compliment</button>
+            <button type="button" onClick={()=>props.functionCompliments()}>{props.text}</button>
         </div>
     );
 };
--- js/components/submit.js
+++ js/components/submit.js
@@ -5,88 +5,55 @@ import ReactDOM from 'react-dom';
 import Display from './display';
 
 export default class Submit extends React.Component {
-constructor(props){
-    super(props);
-    this.state = {
-        clicked: false,
-        compliments: ["You are a better programmer than Ryan Dahl", "You have better ", "You are MORE really really really ridiculously good looking than Derek Zoolander"]
-    }; 
-    this.onButtonClick =this.onButtonClick.bind(this);
-}
+    constructor(props) {
+        super(props);
+    }
 
-onButtonClick(){
-    console.log("On button click");
-    this.setState({clicked: true});
-}
-
-// 
-
-
-
-
-// const showCompliment = () => {
-// return compliments[Math.floor((Math.random() * 3))];
-// };
-
-showCompliment(){
-    return this.state.compliments[Math.floor((Math.random() * 3))]
-}
-
-startApp() {
+    goodCompliment() {
+        console.log('show compliment');
+        var complimentsss = ["You are a better programmer than Ryan Dahl", "You have better ", "You are MORE really really really ridiculously good looking than Derek Zoolander"];
+        complimentsss[Math.floor((Math.random() * 3))]
         return (
             <div>
-                <Display onClick={this.onButtonClick} text="showCompliment"/>
+                <h1>'I am Good'</h1>
+            </div>
+        );
+    }
+
+    badCompliment() {
+        console.log('bad compliment');
+        var complimentsss = ["1", "2", "3"];
+        complimentsss[Math.floor((Math.random() * 3))]
+        return (
+            <div>
+                {'I am Bad'}
+            </div>
+        );
+    }
+
+    startApp() {
+        return (
+            <div>
+                <Display text="goodCompliment" functionCompliments={this.goodCompliment}/>
+                <Display text="badCompliment" functionCompliments={this.badCompliment}/>
             </div>
         )
-}
+    }
 
+    render() {
 
-appClicked(){
-    return (
-        <div>
-            <Display onClick={this.onButtonClick} text="showCompliment"/>
-            {this.showCompliment()}
-        </div>
-    )
-}
+        return this.startApp();
 
-render(){
-// const compliments = ["You are a better programmer than Ryan Dahl", "You have better ", "You are MORE really really really ridiculously good looking than Derek Zoolander"];
-// const showCompliment = () => {
-// return compliments[Math.floor((Math.random() * 3))];
-//  var showCompliment;
-     if(!this.state.clicked){
-         return this.startApp();
-     } else {
-         return this.appClicked();
-     }
     }
 
 }
 
-
-// class Surprise extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {clicked: false};
-//     }
-//     // The callback.  Called when the button is clicked so the
-//     // parent knows that something has happened
-//     onButtonClick() {
-//         this.setState({clicked: true});
-//     }
-//     render() {
-//         var message;
-//         if (this.state.clicked) {
-//             message = "Hello world";
-//         }
-
-//         // Passes the onButtonClick callback to the child 
-//         return (
-//             <div>
-//                 <Button onClick={this.onButtonClick} text="Show message" />
-//                 {message}
-//             </div>
-//         );
-//     }
-// }
+// class Surprise extends React.Component {     constructor(props) {
+// super(props);         this.state = {clicked: false};     }     // The
+// callback.  Called when the button is clicked so the     // parent knows that
+// something has happened     onButtonClick() {         this.setState({clicked:
+// true});     }     render() {         var message;         if
+// (this.state.clicked) {             message = "Hello world";         }
+// // Passes the onButtonClick callback to the child         return (
+//  <div>                 <Button onClick={this.onButtonClick} text="Show
+// message" />                 {message}             </div>         );     } }
